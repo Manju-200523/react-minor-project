0bbe515ee7d09c7f49f8cfea503e6ee2c63e05d4
Manju L <manjugowda200523@gmail.com>
2025-10-30 16:34:05 +0530

    Search-Box added

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 //import './App.css'
-import Button from '@mui/material/Button';
+import SearchBox from './SearchBox'
 
 function App() {
-  let handleclick=()=>{
-    console.log("Button was clicked");
-  };
+  
 
   return (
     <>
-      <h1>Material UI</h1>
-      <Button onClick={handleclick} variant="contained">CLICK ME !</Button>
+      <SearchBox/>
     </>
   )
 }
---
+++ src/SearchBox.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import "./SearchBox.css";
+
+export default function SearchBox() {
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = "c4a595e15889dbd175a496e722a9f34f";
+
+  let getWeatherInfo = async (city) => {
+  try {
+    let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+    let jsonResponse = await response.json();
+    console.log(jsonResponse);
+    let result = {
+      temp: jsonResponse.main.temp,
+      tempMin: jsonResponse.main.temp_min,
+      tempMax: jsonResponse.main.temp_max,
+      humidity: jsonResponse.main.humidity,
+      feelsLike: jsonResponse.main.feels_like,
+      weather: jsonResponse.weather[0].description,
+    };
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+  let [city, setCity] = useState("");
+
+  let handleChange = (evt) => {
+    setCity(evt.target.value);
+  };
+
+  let handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(city);
+    getWeatherInfo(city);
+    setCity("");
+  };
+
+  return (
+    <div className="search-box">
+      <h3>Search for Weather</h3>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="City"
+          label="City Name"
+          variant="outlined"
+          required
+          value={city}
+          onChange={handleChange}
+        />
+        <Button variant="contained" type="submit">
+          Search
+        </Button>
+      </form>
+    </div>
+  );
+
+}
